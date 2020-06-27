589bcdc781ddd137f73b0c684354a548ac8ba698
Yoojae <yooflyhigh@gmail.com>
2020-06-27 14:48:43 +0900

    디렉토리 생성

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Board from '../views/Board.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Board',
+    component: Board
   },
   {
     path: '/about',
